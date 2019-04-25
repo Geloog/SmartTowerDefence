d86ab8ff3ac59e291d93f7c3144f36c462239a04
hasee <951367242@qq.com>
2019-04-25 17:14:18 +0900

    4.15
    加入箭塔与箭的模型，一切开始！

---
+++ Assets/Moving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+
+    private CharacterController controller;
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            controller.SimpleMove(new Vector3(1 * speed, 0,0));
+        if (Input.GetKey(KeyCode.LeftArrow))
+            controller.SimpleMove(new Vector3(-1 * speed, 0, 0));
+        if (Input.GetKey(KeyCode.UpArrow))
+            controller.SimpleMove(new Vector3(0, 0, 1 * speed));
+        if (Input.GetKey(KeyCode.DownArrow))
+            controller.SimpleMove(new Vector3(0, 0, -1 * speed));
+    }
+}
